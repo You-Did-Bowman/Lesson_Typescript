ed06e5ca5837f732473391c2ee5d51ab4730f597
Judith Bohmann <judith.bohmann@dci-student.org>
2025-03-26 12:08:42 +0100

    obejcts, type alias

--- 01intro/myObjects.js
+++ 01intro/myObjects.js
@@ -1,22 +1,13 @@
 // TS ist auf JS aufgebaut ... daher ist es auch nocht object orientated
-// objects sind genauso aufgebaut wir in JS
-var user = {
+var myUser = {
+    _id: "123456",
     name: "Judith",
     email: "judith@mail.com",
     isActive: true,
 };
-// Wenn man mit ihnen arbeitet, muss man für jeden key, den Type mitangeben, weil sonst der default erstellt wird ... "any"
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
-}
-createUser({ name: "judith", isPaid: false });
-// Hier teilen wir mit, dass der Type von return ein object ist ": {}"
-function createCourse(name, price) {
-    return { name: name, price: price };
-}
-console.log(createCourse("react", 50));
-function addUser(user) {
-    return user;
-}
-// Da wir als Type "User" angegeben haben. Muss nun der Parameter auch so aufgebaut sein, wie wir "User" als Type festgelegt haben - als object mit allen benannten keys. 
-console.log(addUser({ name: "judith", email: "judith@mail,com", isActive: true }));
+console.log(myUser);
+// das geht easy
+myUser.email = "newMail";
+// das geht nicht, weil wir es "readonly" gemacht haben
+// myUser._id = "147852"
+console.log(myUser);
--- 01intro/myObjects.ts
+++ 01intro/myObjects.ts
@@ -1,6 +1,6 @@
 // TS ist auf JS aufgebaut ... daher ist es auch nocht object orientated
 
-// objects sind genauso aufgebaut wir in JS
+/* // objects sind genauso aufgebaut wir in JS
 const user = {
     name: "Judith",
     email: "judith@mail.com",
@@ -18,12 +18,12 @@ function createCourse(name: string, price: number): {} {
     return {name, price}
 }
 
-console.log(createCourse("react", 50))
+console.log(createCourse("react", 50)) */
 
 // ------------- TYPE ALIASES ------------------
 
 // Wir können auch unsere eigenen Types erstellen.
-type User = {
+/* type User = {
     name: string;
     email: string;
     isActive: boolean
@@ -33,5 +33,47 @@ function addUser(user: User): User {
     return user
 }
 
-// Da wir als Type "User" angegeben haben. Muss nun der Parameter auch so aufgebaut sein, wie wir "User" als Type festgelegt haben - als object mit allen benannten keys. Das hilft den Code wieder sauberer zu machen und darauf zu achten, dass weniger Fehler entstehen. Er würde nämlich meckern, wenn wir einen key vergessen, der gebraucht wird.
-console.log(addUser({name: "judith", email: "judith@mail,com", isActive: true}))
+// Da wir als Type "User" angegeben haben. Muss nun der Parameter auch so aufgebaut sein, wie wir "User" als Type festgelegt haben - als object mit allen benannten props. Das hilft den Code wieder sauberer zu machen und darauf zu achten, dass weniger Fehler entstehen. Er würde nämlich meckern, wenn wir einen key vergessen, der gebraucht wird.
+console.log(addUser({name: "judith", email: "judith@mail,com", isActive: true})) */
+
+type User = {
+  // readonly macht diesen prop schreibgeschützt. Wir machen das hier mit der Id in MongoDB
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  // "?" macht den prop optional - nicht jeder hat eine Kreditkarte ;)
+  creditcardDetails?: number;
+};
+
+let myUser: User = {
+  _id: "123456",
+  name: "Judith",
+  email: "judith@mail.com",
+  isActive: true,
+};
+
+
+console.log(myUser);
+
+
+type cardNumber = {
+    num: string
+}
+
+type cardDate = {
+    date: string
+}
+
+// Wir können Types aus frankensteinen aus anderen Types.
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
+
+// das geht easy
+myUser.email = "newMail";
+
+// das geht nicht, weil wir es "readonly" gemacht haben
+// myUser._id = "147852"
+
+console.log(myUser)
