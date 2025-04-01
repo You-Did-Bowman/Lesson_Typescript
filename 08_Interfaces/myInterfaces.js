47e7a8ee84793c01772ab0dfacb89db7a25a199e
Judith Bohmann <judith.bohmann@dci-student.org>
2025-04-01 14:15:06 +0200

    edit myInterfaces

--- 08_Interfaces/myInterfaces.js
+++ 08_Interfaces/myInterfaces.js
@@ -1,6 +1,7 @@
+// Wir erstellen hier jetzt einen Costumer und geben die Werte mit, die wir in der Interface als Must-have angegeben haben
 var judith = { dbId: 42, email: "judith@mail.com", userId: 1234, startTrail: function () { return "Trail started"; },
     getCoupon: function (name) { return 10; }
 };
 console.log(judith);
 judith.email = "newMail@mail.com";
-console.log(judith);
+console.log(judith.email);
--- 08_Interfaces/myInterfaces.ts
+++ 08_Interfaces/myInterfaces.ts
@@ -1,3 +1,7 @@
+/* 
+Wir richten einen Prototypen Customer ein. Er hat die Eigenschaften und Keys, die ein gewöhnlicher Customer in unserem Programm hat. Es beschreibt nicht wirklich Werte oder wie Methoden aussehen. 
+Interface erstellt einen Blaupause und ist im wahrsten Sinne des Wortes ein Interface. Im Grunde könnte man es als eine sehr rudimentäre Class sehen.
+*/
 interface Customer {
   readonly dbId: number; // schreibgeschützt
   email: string;
@@ -5,24 +9,36 @@ interface Customer {
   googleId?: string; //optional
 
   // Methoden schreiben 1:
-  startTrail: (/*logic*/) => string;
+  startTrail: () => string;
 
   // Methoden schreiben 2:
-  stopTrail(/*logic*/): string
+  stopTrail?(): string;
 
-  getCoupon(couponname: string): number
+  getCoupon(couponname: string): number;
 }
 
-const judith: Customer = { dbId: 42, email: "judith@mail.com", userId: 1234, startTrail: () => "Trail started",
-  getCoupon: (name: "judithVoucher") => 10
+// Wir erstellen hier jetzt einen Costumer und geben die Werte mit, die wir in der Interface als Must-have angegeben haben
+const judith: Customer = {
+  dbId: 42,
+  email: "judith@mail.com",
+  userId: 1234,
+
+  // den Methoden geben wir jetzt logic, welche zu dem Interface passt
+  startTrail: () => "Trail started",
+  getCoupon: (name: "judithVoucher") => 10,
 };
 
 console.log(judith);
+/* 
+{
+  dbId: 42,
+  email: 'judith@mail.com',
+  userId: 1234,
+  startTrail: [Function: startTrail],
+  getCoupon: [Function: getCoupon]
+} 
+*/
 
 judith.email = "newMail@mail.com";
 
-console.log(judith);
-
-
-
-
+console.log(judith.email); // newMail@mail.com
