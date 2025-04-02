5fe11e2151386489e4b0ca7bb2de49a90fb506a0
Judith Bohmann <judith.bohmann@dci-student.org>
2025-04-02 15:31:22 +0200

    sanity commit

--- 10_Classes/Class_und_Interfaces/dist/importance.js
+++ 10_Classes/Class_und_Interfaces/dist/importance.js
@@ -1,3 +1,22 @@
 "use strict";
+// "implement" sagt, dass diese Class die gleichen Probs wie "TakePhoto" haben soll. Dennoch müssen wir die Probs selber hinschreiben, sie werden nicht vererbt
 class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+// man kann gerne mehr probs hinzufügen ... aber nicht weniger
+// auch kann man mehrere Interfaces implementieren
+class Youtube {
+    constructor(cameraMode, filter, burst, short) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.short = short;
+    }
+    createStory() {
+        console.log("Beste Story ever!");
+    }
 }
